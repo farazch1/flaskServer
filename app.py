4ede779a0010f02e1ac1b04b1cdf42abe47a416b
Mian Faraz <mianfaraz2002@gmail.com>
2026-05-10 13:01:12 +0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def hi():
+    if request.method == 'GET':
+        return "<p>this is test</p>"
+    if request.method == 'POST':
+        print("post is done")
+        return "<p>huy</p>"
